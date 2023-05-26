c0e655d3e0c6a2ad6e543c4bf247a081eadcbf42
@NekkittAY <37750010+NekkittAY@users.noreply.github.com>
2023-05-26 23:51:28 +0300

    Update Least_squares_approximation.py

--- Least_squares_approximation.py
+++ Least_squares_approximation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import linalg
+import matplotlib.pyplot as plt
 from functools import reduce
 
 def MNK(x0,y0,m):
@@ -30,21 +31,65 @@ def MSE(y,a_pred,x):
     res = res/len(y)
     return res
 
+def euclidean_metric(y,a_pred,x):
+    res = 0
+    for i in range(len(y)):
+        res+=(y[i]-y_pred(a_pred,x[i]))**2
+    res = res**0.5
+    return res
+
+def MAE(y,a_pred,x):
+    res = 0
+    for i in range(len(y)):
+        res+=(y[i]-y_pred(a_pred,x[i]))
+    res = res/len(y)
+    return res
+
+def plot(x,y,a):
+    plt.scatter(x,y,color='r')
+    X = np.arange(min(x)-1,max(x)+1,0.01)
+    f = f'{a[0]}'
+    for i in range(1,len(a)):
+        f+=f'+{a[i]}*X**{i}'
+    f = eval(f)
+    plt.plot(X,f,mec='b')
+    plt.xlim(min(x)-1,max(x)+1)
+    plt.ylim(min(y)-1,max(y)+1)
+    plt.grid(True)
+    plt.show()
+
 x = list(map(float, input().split()))
 y = list(map(float, input().split()))
 m = int(input())
 
 result = MNK(x,y,m)
 
-print(*result)
+print(*result, end='\n\n')
+
 for i in range(len(result)):
     if abs(result[i])<0.001:
         print(f'a{i} = 0')
     else:
         print(f'a{i} = {result[i]}')
-        
+
+print("")
+      
 mse = MSE(y,list(result),x)
 if mse>=0.0001:
     print(f'MSE = {mse}')
 else:
     print(f'MSE<0.0001')
+
+mae = MAE(y,list(result),x)
+if mae>=0.0001:
+    print(f'MAE = {mae}')
+else:
+    print(f'MAE<0.0001')
+    
+metric = euclidean_metric(y,list(result),x)
+if mae>=0.0001:
+    print(f'Euclidean Metric = {metric}')
+else:
+    print(f'Euclidean Metric<0.0001')
+
+plot(x,y,list(result))
